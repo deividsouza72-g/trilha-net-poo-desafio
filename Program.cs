7ba515a802928b9143360f1d64df1201efb1375a
Deivid Souza <deividsouza72@gmail.com>
2025-11-07 11:18:35 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("789", "708090", "456789", 2000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("123", "102030,", "123456", 1000);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
